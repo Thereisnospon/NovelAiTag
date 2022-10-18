fe18f07e91883043d11a39065a1ac915bb0ed2de
thereisnospon <thereisnospon@qq.com>
2022-10-18 23:33:43 +0800

    fix auto

--- injectNeg.js
+++ injectNeg.js
@@ -1,39 +1,42 @@
 
+
 function setToPromptNeg(id) {
 
 
     let apps = document.getElementsByTagName("gradio-app");
-    console.log("s0");
     if (apps == null || apps.length <= 0) {
         return
     }
-    console.log("s02");
     let app = apps[0];
     if (app == null) {
         return;
     }
-    console.log("s03");
     let shadowRoot = app.shadowRoot;
     if (shadowRoot == null) {
         return;
     }
-    console.log("s04");
     let promptDiv = shadowRoot.getElementById(id);
     if (promptDiv != null) {
-        console.log("s05");
         let tags = promptDiv.getElementsByTagName("textarea")
         if (tags != null && tags.length === 1) {
-            console.log("s06");
             if (window.novelAi_inject_neg != null) {
-                tags[0].value = window.novelAi_inject_neg;
-                console.log("s07");
+                setNativeValue(tags[0],window.novelAi_inject_neg)
             }
 
         }
     }
 
 }
-
+function setNativeValue(element, value) {
+    var evt = new InputEvent('input', {
+        inputType: 'insertText',
+        data: value,
+        dataTransfer: null,
+        isComposing: false
+    });
+    element.value = value;
+    element.dispatchEvent(evt);
+}
 setToPromptNeg("txt2img_neg_prompt")
 setToPromptNeg("img2img_neg_prompt")
 window.novelAi_inject_neg=null;
